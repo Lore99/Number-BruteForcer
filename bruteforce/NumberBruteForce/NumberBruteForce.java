c2bacebe4c2e792bec772c5b2f9e7e1ae830d482
Lore99 <34354838+Lore99@users.noreply.github.com>
2017-12-08 02:09:25 +0100

    Add files via upload

--- bruteforce/NumberBruteForce/NumberBruteForce.java
+++ bruteforce/NumberBruteForce/NumberBruteForce.java
@@ -4,7 +4,7 @@ Copyright 2017 Lorenzo Boldorini
 NumberBruteForce is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 NumberBruteForce is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU Affero General Public License for more details.
-You should have received a copy of the GNU General Public License along with MadelineProto.
+You should have received a copy of the GNU General Public License along with NumberBruteForce.
 If not, see <http://www.gnu.org/licenses/>.
 */
 package bruteforce.NumberBruteForce;
